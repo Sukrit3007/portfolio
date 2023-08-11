fa530764eb67b21a7245735a345a062e748fcad0
Sukrit <sukrit3007@outlook.com>
2023-08-11 19:12:27 +0530

    fixed footer link

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <div className="footer-content">
         <h2>“CONNECT ON”</h2>
         <div className="social-links">
-          <a href="www.linkedin.com/in/sukrit37" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/sukrit37" target="_blank" rel="noopener noreferrer">
             <button>LinkedIn</button>
           </a>
           <a href="https://github.com/Sukrit3007" target="_blank" rel="noopener noreferrer">
